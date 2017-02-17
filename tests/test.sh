cdbe1de7d38483457a0b7d2027cf737812ff1c08
kraeml <kraeml@sesamestreet.franken.de>
2017-02-17 17:34:39 +0100

    Display default debian website

--- tests/test.sh
+++ tests/test.sh
@@ -6,6 +6,7 @@ ansible-playbook playbook.yml --connection=local || exit 1
 
 sleep 2
 curl -s http://localhost/phpmyadmin/ | grep -q phpMyAdmin && echo "phpMyAdmin works" || { echo "phpMyAdmin does not work" && exit 1; }
+curl -s http://localhost | grep  -q 'Welcome to nginx' && echo "nginx works" || { echo "nginx does not work" && exit 1; }
 
 ansible-playbook playbook.yml --connection=local | tee /tmp/idempotence.log
 sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" /tmp/idempotence.log | grep -q "changed=0.*failed=0" \
